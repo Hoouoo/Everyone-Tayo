382d59e525b67f498b79cda46ce9ec5450f5d0ee
Sanghoon <shpusan001@gmail.com>
2021-08-12 22:21:14 +0900

    feat Apply Pusha to bus application
    내용: 버스 어플리케이션에 Pusha 적용

--- Everyone-Tayo-BusApplication/app/src/main/java/team/sw/everyonetayo/configuration/PushaConfig.kt
+++ Everyone-Tayo-BusApplication/app/src/main/java/team/sw/everyonetayo/configuration/PushaConfig.kt
@@ -1,7 +1,10 @@
 package team.sw.everyonetayo.configuration;
 
 import pusha.client.manager.ClientManager
+import pusha.packet.Packet
 import pusha.service.ClientPacketRecieveService
+import pusha.service.default_order.Order
+import pusha.socket.WrappedSocket
 
 public class PushaConfig {
     companion object{
@@ -10,5 +13,19 @@ public class PushaConfig {
 
     constructor(){
         if(ClientManager.instance==null) ClientManager.use();
+        ClientPacketRecieveService.instance.addOrder("RESERVATION_NOTICE" ,ReceptionNotice())
+    }
+
+    /**
+     *
+     */
+
+    inner class ReceptionNotice: Order {
+        override fun excute(wrappedSocket: WrappedSocket, data: Any?) {
+            val packet: Packet = data as Packet;
+            val busStop:String = packet.data.toString();
+
+            //TODO Implement yet
+        }
     }
 }
