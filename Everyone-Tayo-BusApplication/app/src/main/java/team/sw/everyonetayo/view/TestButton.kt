2554d9e53a0ed32745962333bdfcd1df1f9bb7c0
JeongGyuEop <gueeop0219@naver.com>
2021-08-13 16:54:40 +0900

    feat Test TTS, Bus UI
    내용 : 사용자 애플리케이션 TTS  구현 및 버스 애플리케이션 UI 구현

--- Everyone-Tayo-BusApplication/app/src/main/java/team/sw/everyonetayo/view/TestButton.kt
+++ Everyone-Tayo-BusApplication/app/src/main/java/team/sw/everyonetayo/view/TestButton.kt
@@ -2,11 +2,10 @@ package team.sw.everyonetayo.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import team.sw.everyonetayo.R
 
-class KeyPad : AppCompatActivity() {
+class TestButton : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_key_pad)
+        setContentView(R.layout.activity_test_button)
     }
 }
