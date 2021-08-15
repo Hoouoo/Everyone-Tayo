8b6a1b8d728cbcbcd85bdaf5d77dd16b8ea07d07
JeongGyuEop <gueeop0219@naver.com>
2021-08-15 22:55:40 +0900

    feat Reservation list
    내용 : 버스 애플리케이션 UI 구현 및 리스트 기능 구현

--- Everyone-Tayo-Application/app/src/main/java/team/sw/everyonetayo/view/MainActivity.kt
+++ Everyone-Tayo-Application/app/src/main/java/team/sw/everyonetayo/view/MainActivity.kt
@@ -1,27 +1,71 @@
 package team.sw.everyonetayo.view
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.speech.tts.TextToSpeech
 import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import team.sw.everyonetayo.R
-import team.sw.everyonetayo.view.login.LoginActivity
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    val DURATION:Long = 1000 //액티비티 이동의 1.5초 딜레이
+    private var tts: TextToSpeech? = null //tts 사용
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        start_button.setOnClickListener{
-            val intent = Intent(this, SelectService::class.java)
-            startActivity(intent)
+
+        tts = TextToSpeech(this, this)
+
+        start_button.setOnClickListener {
+            // tts 음성 말하기
+            speakOut("시작하기")
+            // tts 음성 출력 후 액티비티 이동을 위한 딜레이
+            Handler().postDelayed({
+                val intent = Intent(this, SelectService::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+            }, DURATION)
         }
 
-
-        var actionBar : ActionBar?
-        actionBar = supportActionBar
+        // 상단 액션바 숨김
+        val actionBar : ActionBar? = supportActionBar
         actionBar?.hide()
 
     }
+
+    // TextToSpeech.OnInitListener를 상속하는 과정에서 필요한 부분
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.KOREAN)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
+
+    // tts 음성을 출력하기 위한 함수
+    private fun speakOut(text_s : String) {
+        tts!!.speak(text_s, TextToSpeech.QUEUE_FLUSH, null,"")
+    }
+
+    // tts 음성을 초기화 해주기 위한 함수
+    public override fun onDestroy() {
+        // Shutdown TTS
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+
 }
--- Everyone-Tayo-Application/app/src/main/java/team/sw/everyonetayo/view/VoiceReader.kt
+++ Everyone-Tayo-Application/app/src/main/java/team/sw/everyonetayo/view/VoiceReader.kt
@@ -3,21 +3,41 @@ package team.sw.everyonetayo.view
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_voice_reader.*
 import team.sw.everyonetayo.R
+import java.util.*
 
-class VoiceReader : AppCompatActivity() {
+class VoiceReader : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    val DURATION:Long = 1000 //액티비티 이동의 1.5초 딜레이
+    private var tts: TextToSpeech? = null //tts 사용
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_voice_reader)
-
+        tts = TextToSpeech(this, this)
         setTitle("음성인식 버스 예약")
 
+        voice_reader.setOnClickListener{
+            // tts 음성 말하기
+            speakOut("음성 인식. 띵똥 소리 후, 버스 번호를 말해주세요")
+            // 음성 인식을 진행하기 위한 함수 필요
+        }
+
         keypad_call.setOnClickListener{
-            val intent = Intent(this, KeyPad::class.java)
-            startActivity(intent)
-            finish()
+            // tts 음성 말하기
+            speakOut("키패드 입력")
+            // tts 음성 출력 후 액티비티 이동을 위한 딜레이
+            Handler().postDelayed({
+                val intent = Intent(this, KeyPad::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                finish()
+            }, DURATION)
         }
 
         //뒤로가기 버튼
@@ -36,5 +56,32 @@ class VoiceReader : AppCompatActivity() {
                 return super.onOptionsItemSelected(item)
             }
         }
+    }
+    // TextToSpeech.OnInitListener를 상속하는 과정에서 필요한 부분
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.KOREAN)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
+
+    // tts 음성을 출력하기 위한 함수
+    private fun speakOut(text_s : String) {
+        tts!!.speak(text_s, TextToSpeech.QUEUE_FLUSH, null,"")
+    }
+
+    // tts 음성을 초기화 해주기 위한 함수
+    public override fun onDestroy() {
+        // Shutdown TTS
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
     }
 }
