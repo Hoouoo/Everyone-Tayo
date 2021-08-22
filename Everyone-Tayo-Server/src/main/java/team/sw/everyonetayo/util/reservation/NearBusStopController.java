bcf3aa3a9112a1d4fc7c6c9dbb17fb09578cedd4
sonmin23 <outro59@naver.com>
2021-08-22 22:56:03 +0900

    feat bus reservation 내용 : 버스 예약시 버스 번호 중복 제거 #39

--- Everyone-Tayo-Server/src/main/java/team/sw/everyonetayo/util/reservation/NearBusStopController.java
+++ Everyone-Tayo-Server/src/main/java/team/sw/everyonetayo/util/reservation/NearBusStopController.java
@@ -75,54 +75,58 @@ public class NearBusStopController {
         // 가장 가까운 버스 정류소 추출
 
         // 사용자가 입력한 버스 번호를 토대로 DB 조회
-        List<String> targetRoute = new ArrayList<>();
-        busRouteRepository.findAllByRouteNo(requestNearBusDto.getBusNumber()).forEach(item -> targetRoute.add(item.getRouteId()));
-        System.out.println("targetRoute = " + targetRoute.get(0));
+        List<String> targetRoutes = new ArrayList<>();
+        busRouteRepository.findAllByRouteNo(requestNearBusDto.getBusNumber()).forEach(item -> targetRoutes.add(item.getRouteId()));
 
-        String targetRouteId = busRouteRepository.findByRouteNo(requestNearBusDto.getBusNumber())
-                .orElseThrow(() -> new NoSuchRouteIdException("존재하지 않는 RouteId 입니다.")).getRouteId();
+//        String targetRouteId = busRouteRepository.findByRouteNo(requestNearBusDto.getBusNumber())
+//                .orElseThrow(() -> new NoSuchRouteIdException("존재하지 않는 RouteId 입니다.")).getRouteId();
 //        System.out.println("targetRouteId = " + targetRouteId);
-
+        String targetRouteId = null;
         // 동일한 노선 Id를 가지는 버스 집합
         List<ResponseBusLocationDto> busList = new ArrayList<>();
         // Step2. 가장 가까운 버스 정류소의 버스 노선 Id 추츨
-        try {
-            String apiUrl = "http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/getRouteAcctoThrghSttnList?"
-                    + "ServiceKey=tO6fJs7AxOJ%2Bf9N5nWEgSE16%2BuOewB1LlIMM%2Fs5NB6bHtZ%2B3iO%2BcOIKgzK4QrYfZmIzh0iwJ1XKdbhxKEK2FtA%3D%3D"
-                    + "&numOfRows=100"
-                    + "&pageNo=1"
-                    + "&cityCode=" + responseNearBusDto.getCityCode()
-                    + "&routeId=" + targetRouteId;
+        for(String targetRoute: targetRoutes) {
+            System.out.println("targetRoute = " + targetRoute);
+            try {
+                String apiUrl = "http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/getRouteAcctoThrghSttnList?"
+                        + "ServiceKey=tO6fJs7AxOJ%2Bf9N5nWEgSE16%2BuOewB1LlIMM%2Fs5NB6bHtZ%2B3iO%2BcOIKgzK4QrYfZmIzh0iwJ1XKdbhxKEK2FtA%3D%3D"
+                        + "&numOfRows=100"
+                        + "&pageNo=1"
+                        + "&cityCode=" + responseNearBusDto.getCityCode()
+                        + "&routeId=" + targetRoute;
 
-            System.out.println("targetResponseNearBusDto = " + responseNearBusDto.getCityCode());
-            URL url = new URL(apiUrl);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.connect();
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
-            String returnLine;
-            while ((returnLine = bufferedReader.readLine()) != null) {
-                result.append(returnLine);
-            }
-            JSONObject jsonObject = XML.toJSONObject(result.toString());
-            System.out.println("#1 jsonObject.toString() = " + jsonObject);
-            result.setLength(0);
-            JSONObject responseObject = (JSONObject) jsonObject.get("response");
-            JSONObject bodyObject = (JSONObject) responseObject.get("body");
-            JSONObject itemObject = (JSONObject) bodyObject.get("items");
-            jsonPrintString.append(itemObject.toString());
-            JSONArray item = (JSONArray) itemObject.get("item");
-            for (int i = 0; i < item.length(); i++) {
-                JSONObject targetItem = (JSONObject) item.get(i);
-                if (responseNearBusDto.getNodeId().equals(targetItem.get("nodeid"))) {
-                    busWayPoint = new ResponseRouteWayPointDto.ResponseRouteWayPointDtoBuilder()
-                            .nodeId(String.valueOf(targetItem.get("nodeid")))
-                            .nodeOrd(String.valueOf(targetItem.get("nodeord")))
-                            .build();
+                System.out.println("targetResponseNearBusDto = " + responseNearBusDto.getCityCode());
+                URL url = new URL(apiUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
+                String returnLine;
+                while ((returnLine = bufferedReader.readLine()) != null) {
+                    result.append(returnLine);
                 }
+                JSONObject jsonObject = XML.toJSONObject(result.toString());
+                System.out.println("#1 jsonObject.toString() = " + jsonObject);
+                result.setLength(0);
+                JSONObject responseObject = (JSONObject) jsonObject.get("response");
+                JSONObject bodyObject = (JSONObject) responseObject.get("body");
+                JSONObject itemObject = (JSONObject) bodyObject.get("items");
+                jsonPrintString.append(itemObject.toString());
+                JSONArray item = (JSONArray) itemObject.get("item");
+                for (int i = 0; i < item.length(); i++) {
+                    JSONObject targetItem = (JSONObject) item.get(i);
+                    if (responseNearBusDto.getNodeId().equals(targetItem.get("nodeid"))) {
+                        targetRouteId = targetRoute;
+                        busWayPoint = new ResponseRouteWayPointDto.ResponseRouteWayPointDtoBuilder()
+                                .nodeId(String.valueOf(targetItem.get("nodeid")))
+                                .nodeOrd(String.valueOf(targetItem.get("nodeord")))
+                                .build();
+                        System.out.println("busWayPoint = " + busWayPoint);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         // 가장 가까운 버스 정류소의 버스 노선 Id 추츨
 
